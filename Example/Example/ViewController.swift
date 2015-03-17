1be7d4a72a87bee5c1d0aebe75f70ca05da2e896
Jesse Squires <jesse.d.squires@gmail.com>
2015-03-17 16:59:01 -0700

    add initial framework proj

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import JSQCoreDataKit
 
 class ViewController: UIViewController {
 
---
+++ JSQCoreDataKit/JSQCoreDataKit/JSQCoreDataKit.h
@@ -0,0 +1,19 @@
+//
+//  JSQCoreDataKit.h
+//  JSQCoreDataKit
+//
+//  Created by Jesse Squires on 3/17/15.
+//  Copyright (c) 2015 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSQCoreDataKit.
+FOUNDATION_EXPORT double JSQCoreDataKitVersionNumber;
+
+//! Project version string for JSQCoreDataKit.
+FOUNDATION_EXPORT const unsigned char JSQCoreDataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSQCoreDataKit/PublicHeader.h>
+
+

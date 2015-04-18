12d06594f3b60c56aa8b5db2734aa1b64dadd656
Jesse Squires <jesse.d.squires@gmail.com>
2015-04-18 13:17:41 -0700

    comments

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -23,6 +23,25 @@ import ExampleModel
 
 import JSQCoreDataKit
 
+/*
+
+Here's a terrible, Obj-C-ish way to do things.
+
+extension NSManagedObject {
+
+    class func entityName() -> String {
+        let fullClassName = NSStringFromClass(object_getClass(self))
+        let classNameComponents: [String] = split(fullClassName) { $0 == "." }
+        return last(classNameComponents)!
+    }
+
+    convenience init(context: NSManagedObjectContext) {
+        let entityDescription = NSEntityDescription.entityForName(self.dynamicType.entityName(), inManagedObjectContext: context)!
+        self.init(entity: entityDescription, insertIntoManagedObjectContext: context)
+    }
+}
+
+*/
 
 class ViewController: UIViewController {
 

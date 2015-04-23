62733539f1d1c4b3581b764dffead4aa8ebe12c0
Jesse Squires <jesse.d.squires@gmail.com>
2015-04-23 12:52:48 -0700

    update docs and build script for latest jazzy. close #7

--- build_docs.sh
+++ build_docs.sh
@@ -5,6 +5,4 @@
 # https://github.com/realm/jazzy
 # ------------------------------
 
-cd JSQCoreDataKit
-jazzy -o ../_docs -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQCoreDataKit' -g 'https://github.com/jessesquires/JSQCoreDataKit'
-cd ..
+jazzy -o ./_docs --source-directory ./JSQCoreDataKit --readme ./README.md -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQCoreDataKit' -g 'https://github.com/jessesquires/JSQCoreDataKit'

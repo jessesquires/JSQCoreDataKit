bf4bb4a52e38c8534b39cbbecd90803db0cf4654
Jesse Squires <jesse.d.squires@gmail.com>
2015-10-10 20:10:26 -0700

    add new example app project

---
+++ ExampleApp/ExampleApp/EmployeeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/JSQCoreDataKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQCoreDataKit
+//
+//
+//  License
+//  Copyright © 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import UIKit
+import CoreData
+
+import JSQCoreDataKit
+
+import ExampleModel
+
+
+
+class EmployeeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ ExampleApp/ExampleAppTests/ExampleAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/JSQCoreDataKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQCoreDataKit
+//
+//
+//  License
+//  Copyright © 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import XCTest
+@testable import ExampleApp
+
+
+class ExampleAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}

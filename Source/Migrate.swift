68a1fcacbc3e54e39b2592d11a5274ecc53c2e5a
Marius <marius-serban@users.noreply.github.com>
2016-04-02 23:05:53 +0100

    Add top level function for migrations. Close #46.

---
+++ Source/Migrate.swift
@@ -0,0 +1,113 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/JSQCoreDataKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQCoreDataKit
+//
+//
+//  License
+//  Copyright © 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import CoreData
+
+public enum MigrationError: ErrorType {
+    case SourceManagedObjectModelNotFound
+    case MappingModelNotFound(sourceManagedObjectModel: NSManagedObjectModel)
+}
+
+/**
+ Progressively migrates the persistent store of a `CoreDataModel` based on mapping models found in the model's bundle.
+
+ - note: Migration is only supported for on-disk persistent stores.
+ A complete 'path' of mapping models must exist between the peristent store's version and the model's version
+
+ - parameter model:      The `CoreDataModel` to perform migration upon.
+
+ - throws: If the `NSManagedObjectModel` corresponding to the existing persistent store is not found in the model bundle then this function throws `.SourceManagedObjectModelNotFound`. If an `NSMappingModel` part of the progressive migration 'path' is not found in the model bundle then a `.MappingModelNotFound` is thrown. The function rethrows exceptions that may result from reading the persistent store metadata, file system operations or migration with `NSMigrationManager`.
+ */
+public func migrate(model: CoreDataModel) throws {
+
+    guard model.needsMigration else { return }
+
+    guard let storeURL = model.storeURL else {
+        preconditionFailure("Migration is only available for on-disk persistent stores")
+    }
+
+    let storeType = model.storeType.type
+
+    guard let sourceModel = try findModelCompatibleWithStore(model.bundle, storeType: storeType, storeURL: storeURL) else {
+        throw MigrationError.SourceManagedObjectModelNotFound
+    }
+
+    let migrationSteps = try buildMappingModelPath(model.bundle, sourceModel: sourceModel, destinationModel: model.managedObjectModel)
+
+    for step in migrationSteps {
+        let tempURL = DefaultDirectoryURL().URLByAppendingPathComponent("migration.sqlite")
+
+        let manager = NSMigrationManager(sourceModel: step.source, destinationModel: step.destination)
+        try manager.migrateStoreFromURL(storeURL, type: storeType, options: nil, withMappingModel: step.mapping, toDestinationURL: tempURL, destinationType: storeType, destinationOptions: nil)
+
+        try model.removeExistingModelStore()
+        try NSFileManager.defaultManager().moveItemAtURL(tempURL, toURL: storeURL)
+    }
+
+}
+
+
+// MARK: Internal
+
+internal func findModelCompatibleWithStore(bundle: NSBundle, storeType: String, storeURL: NSURL) throws -> NSManagedObjectModel? {
+    let storeMetadata = try NSPersistentStoreCoordinator.metadataForPersistentStoreOfType(storeType, URL: storeURL, options: nil)
+
+    for model in findModelsInBundle(bundle) where model.isConfiguration(nil, compatibleWithStoreMetadata: storeMetadata) {
+        return model
+    }
+
+    return nil
+}
+
+internal func findModelsInBundle(bundle: NSBundle) -> [NSManagedObjectModel] {
+    guard let modelDirectoryURLs = bundle.URLsForResourcesWithExtension("momd", subdirectory: nil) else {
+        return []
+    }
+
+    let modelDirectoryNames = modelDirectoryURLs.flatMap { $0.lastPathComponent }
+
+    let modelURLs = Array(modelDirectoryNames.flatMap { bundle.URLsForResourcesWithExtension("mom", subdirectory: $0) }.flatten())
+
+    return modelURLs.flatMap { NSManagedObjectModel(contentsOfURL: $0) }
+}
+
+internal func buildMappingModelPath(bundle: NSBundle, sourceModel: NSManagedObjectModel, destinationModel: NSManagedObjectModel) throws -> [(source: NSManagedObjectModel, mapping: NSMappingModel, destination: NSManagedObjectModel)] {
+    var migrationSteps: [(source: NSManagedObjectModel, mapping: NSMappingModel, destination: NSManagedObjectModel)] = []
+    var stepModel: NSManagedObjectModel = sourceModel
+    repeat {
+        guard let nextModelAndMapping = nextIncrementalModelAndMapping(sourceModel: stepModel, bundle: bundle) else {
+            throw MigrationError.MappingModelNotFound(sourceManagedObjectModel: stepModel)
+        }
+
+        migrationSteps.append((source: stepModel, mapping: nextModelAndMapping.mapping, destination: nextModelAndMapping.destination))
+        stepModel = nextModelAndMapping.destination
+
+    } while stepModel != destinationModel
+
+    return migrationSteps
+}
+
+internal func nextIncrementalModelAndMapping(sourceModel sourceModel: NSManagedObjectModel, bundle: NSBundle) -> (destination: NSManagedObjectModel, mapping: NSMappingModel)? {
+    for nextDestinationModel in findModelsInBundle(bundle) where nextDestinationModel != sourceModel {
+        if let mappingModel = NSMappingModel(fromBundles: [bundle], forSourceModel: sourceModel, destinationModel: nextDestinationModel) {
+            return (nextDestinationModel, mappingModel)
+        }
+    }
+
+    return nil
+}
---
+++ Tests/MigrationTests.swift
@@ -0,0 +1,184 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/JSQCoreDataKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQCoreDataKit
+//
+//
+//  License
+//  Copyright © 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import XCTest
+import CoreData
+
+import ExampleModel
+
+@testable
+import JSQCoreDataKit
+
+
+class MigrationTests: TestCase {
+
+    lazy var model: CoreDataModel = CoreDataModel(name: modelName, bundle: modelBundle, storeType: .SQLite(DefaultDirectoryURL()))
+
+    override func setUp() {
+        super.setUp()
+
+        try! model.removeExistingModelStore()
+    }
+
+    func test_GivenExistingPersistentStoreIsStale_ThenCoreDataModel_needsMigration() {
+        // GIVEN: an existing SQLite file with metadata pointing to an old version of the model
+        createSQLitePersistentStore(managedObjectModelVersion("Version 1"))
+
+        // THEN: model needs migration
+        XCTAssertTrue(model.needsMigration)
+    }
+
+    func test_GivenExistingPersistentStoreIsFresh_ThenCoreDataModel_doesNotNeedMigration() {
+        // GIVEN: an existing SQLite file with metadata pointing to the latest version of the model
+        createSQLitePersistentStore(managedObjectModelVersion("Version 3"))
+
+        // THEN: model does not need migration
+        XCTAssertFalse(model.needsMigration)
+    }
+
+
+    func test_GivenStalePersistentStore_WhenModelIsMigrated_ThenModelDoesNotNeedMigration() {
+        // GIVEN: an existing SQLite file with metadata pointing to an old version of the model
+        createSQLitePersistentStore(managedObjectModelVersion("Version 1"))
+
+        // WHEN: CoreDataModel is migrated
+        try! migrate(model)
+
+        // THEN: model does not need migration
+        XCTAssertFalse(model.needsMigration)
+    }
+
+    func test_GivenExistingStore_WhenFindingItsModel_ThenTheCorrectModelIsFound() {
+        // GIVEN: a SQLite store with metadata pointing to a specific version of the model
+        let version1Model = managedObjectModelVersion("Version 1")
+        let persistentStore = createSQLitePersistentStore(version1Model)
+
+        // WHEN: the compatible model is found in the test bundle
+        let foundModel = try! findModelCompatibleWithStore(model.bundle, storeType: persistentStore.type, storeURL: persistentStore.URL!)
+
+        // THEN: the found model is the same as the one used to create the store
+        XCTAssertEqual(foundModel, version1Model)
+    }
+
+    func test_WhenFetchinglAllModelVersions_AllVersionsAreFound() {
+        // WHEN: fetching all model versions from the test bundle
+        let modelsInBundle = findModelsInBundle(model.bundle)
+
+        // THEN: all model versions are found
+        let version1 = managedObjectModelVersion("Version 1")
+        let version2 = managedObjectModelVersion("Version 2")
+        let version3 = managedObjectModelVersion("Version 3")
+        XCTAssertEqual(Set(modelsInBundle), Set([version1, version2, version3]))
+    }
+
+    func test_GivenSourceAndDestinationModels_WhenBuildingTheMappingPathsBetweenThem_ThenTheMappingStepsAreCorrect() {
+        // GIVEN: the source and destination models
+        let sourceModel = managedObjectModelVersion("Version 1")
+        let destinationModel = managedObjectModelVersion("Version 3")
+
+        // WHEN: building the mapping path
+        let mappingPath = try! buildMappingModelPath(model.bundle, sourceModel: sourceModel, destinationModel: destinationModel)
+
+        // THEN: the mapping steps are correct
+        let version2Model = managedObjectModelVersion("Version 2")
+        let version1ToVersion2Mapping = mappingModel("Version1_to_Version2")
+        let version2ToVersion3Mapping = mappingModel("Version2_to_Version3")
+
+        XCTAssertEqual(mappingPath[0].source, sourceModel)
+        XCTAssertEqual(mappingPath[0].mapping, version1ToVersion2Mapping)
+        XCTAssertEqual(mappingPath[0].destination, version2Model)
+
+        XCTAssertEqual(mappingPath[1].source, version2Model)
+        XCTAssertEqual(mappingPath[1].mapping, version2ToVersion3Mapping)
+        XCTAssertEqual(mappingPath[1].destination, destinationModel)
+    }
+
+    func test_GivenTwoModelsWithNoMappingPath_WhenBuildingTheMappingPathsBetweenThem_ThenExceptionIsThrown() {
+        // GIVEN: the source and destination models
+        let sourceModel = managedObjectModelVersion("Version 1")
+        let destinationModel = sourceModel
+
+        // WHEN: building the mapping path
+        do {
+            try buildMappingModelPath(model.bundle, sourceModel: sourceModel, destinationModel: destinationModel)
+
+        // THEN: a `MigrationError.MappingModelNotFound` exception is thrown
+        } catch MigrationError.MappingModelNotFound(let errorModel) {
+            XCTAssertEqual(errorModel, managedObjectModelVersion("Version 3"))
+            return
+
+        } catch { }
+
+        XCTFail("Expected exception not thrown")
+    }
+
+    func test_GivenModelWithForwardMapping_WhenFetchingTheNextMappingAndModel_ThenTheCorrectPairIsFound() {
+        // GIVEN: a model that has a corresponding mapping model
+        let version1Model = managedObjectModelVersion("Version 1")
+
+        // WHEN: finding the next model and mapping
+        let (targetModel, mapping) = nextIncrementalModelAndMapping(sourceModel: version1Model, bundle: model.bundle)!
+
+        // THEN: the correct mapping and target model are found
+        XCTAssertEqual(mapping, mappingModel("Version1_to_Version2"))
+        XCTAssertEqual(targetModel, managedObjectModelVersion("Version 2"))
+    }
+
+    func test_GivenModelWithNoForwardMapping_WhenFindingTheNextMappingAndModel_ThenNilIsReturned() {
+        // GIVEN: a model that has a corresponding mapping model
+        let version3Model = managedObjectModelVersion("Version 3")
+
+        // WHEN: finding the next model and mapping
+        let result = nextIncrementalModelAndMapping(sourceModel: version3Model, bundle: model.bundle)
+
+        // THEN: the return value is nil
+        XCTAssertNil(result)
+    }
+
+    // MARK: Helpers
+
+    func createSQLitePersistentStore(managedObjectModel: NSManagedObjectModel) -> NSPersistentStore {
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        let persistentStoreURL = DefaultDirectoryURL().URLByAppendingPathComponent("\(modelName).sqlite")
+
+        return try! persistentStoreCoordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: persistentStoreURL, options: nil)
+    }
+
+    func managedObjectModelVersion(name: String) -> NSManagedObjectModel {
+        let modelURL = model.modelURL.URLByAppendingPathComponent("\(name).mom")
+
+        guard let result = NSManagedObjectModel(contentsOfURL: modelURL) else {
+            preconditionFailure("Model with given name not found in bundle or is invalid.")
+        }
+
+        return result
+    }
+
+    func mappingModel(name: String) -> NSMappingModel {
+        guard let mappingModelURL = model.bundle.URLForResource(name, withExtension: "cdm") else {
+            preconditionFailure("Mapping model named \(name) not found in bundle.")
+        }
+
+        guard let result = NSMappingModel(contentsOfURL: mappingModelURL) else {
+            preconditionFailure("Mapping model named \(name) is invalid.")
+        }
+
+        return result
+    }
+
+}

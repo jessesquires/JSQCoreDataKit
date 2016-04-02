ed1c9503a052488e6fe08efc3aaae0c81a752669
Jesse Squires <jesse.d.squires@gmail.com>
2016-04-02 14:51:08 -0700

    add package.swift. close #55

---
+++ Package.swift
@@ -0,0 +1,23 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/JSQCoreDataKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQCoreDataKit
+//
+//
+//  License
+//  Copyright © 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "JSQCoreDataKit"
+)

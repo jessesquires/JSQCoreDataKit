6bea7043b232da28a55fd1fa9675aa30e92e36c5
Jesse Squires <jesse@jessesquires.com>
2018-10-30 15:57:28 -0700

    fix appdelegate method

--- Example/ExampleApp/AppDelegate.swift
+++ Example/ExampleApp/AppDelegate.swift
@@ -23,8 +23,8 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    private func application(_ application: UIApplication,
-                             didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         return true
     }
 }

1a84e721c7655bc9e4ff18b4b3658ee51fce1058
Jesse Squires <jesse@jessesquires.com>
2020-04-20 16:43:44 -0700

    update build docs script

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Docs by jazzy
-# https://github.com/realm/jazzy
-# ------------------------------
+#  Created by Jesse Squires
+#  https://www.jessesquires.com
+#
+#  Copyright © 2020-present Jesse Squires
+#
+#  Docs by jazzy
+#  https://github.com/realm/jazzy
+#  ------------------------------
+
+PROJECT="JSQCoreDataKit"
 
 if which jazzy >/dev/null; then
     jazzy \
         --clean \
-        --author 'Jesse Squires' \
-        --author_url 'https://twitter.com/jesse_squires' \
-        --github_url 'https://github.com/jessesquires/JSQCoreDataKit' \
-        --module 'JSQCoreDataKit' \
+        --author "Jesse Squires" \
+        --author_url "https://jessesquires.com" \
+        --github_url "https://github.com/jessesquires/$PROJECT" \
+        --module "$PROJECT" \
         --source-directory . \
-        --readme 'README.md' \
-        --documentation 'Guides/*.md' \
+        --readme "README.md" \
+        --documentation "Guides/*.md" \
         --output docs/
         exit
 else

e32dab625724deb3394a8467288ecc897e6180a7
Jesse Squires <jesse@jessesquires.com>
2020-04-21 17:59:13 -0700

    Support SwiftPM. Close #155

--- Package.swift
+++ Package.swift
@@ -34,9 +34,15 @@ let package = Package(
         .target(
             name: "JSQCoreDataKit",
             path: "Sources"),
-        .testTarget(name: "JSQCoreDataKitTests",
-                    dependencies: ["JSQCoreDataKit"],
-                    path: "Tests")
+
+        // Unfortunately, we cannot include tests right now.
+        // The test target depends on a fixture project, `ExampleModel`.
+        // There is not a great way to model that in SwiftPM.
+        // We do not want to include `ExampleModel` in our package.
+        // However, this is not that bad.
+        // We still run tests via the main Xcode project.
+
+        // .testTarget(name: "JSQCoreDataKitTests", dependencies: ["JSQCoreDataKit"], path: "Tests")
     ],
     swiftLanguageVersions: [.v5]
 )

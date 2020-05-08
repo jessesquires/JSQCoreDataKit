8e85642921079ed8bf7e5ebc3698bfb57af05eae
Jesse Squires <jessesquires@users.noreply.github.com>
2020-05-08 16:52:33 -0700

    [CI] update workflows, bundle update, SwiftPM Support (#163)
    - Update workflows to Xcode 11.4.1
    - Bundle update
    - SwiftPM Support, close #155
    - Update jazzy

--- Package.swift
+++ Package.swift
@@ -26,17 +26,19 @@ let package = Package(
         .macOS(.v10_12)
     ],
     products: [
-        .library(
-            name: "JSQCoreDataKit",
-            targets: ["JSQCoreDataKit"])
+        .library(name: "JSQCoreDataKit", targets: ["JSQCoreDataKit"])
     ],
     targets: [
-        .target(
-            name: "JSQCoreDataKit",
-            path: "Sources"),
-        .testTarget(name: "JSQCoreDataKitTests",
-                    dependencies: ["JSQCoreDataKit"],
-                    path: "Tests")
+        .target(name: "JSQCoreDataKit", path: "Sources")
+
+        // Unfortunately, we cannot include tests right now.
+        // The test target depends on a fixture project, `ExampleModel`.
+        // There is not a great way to model that in SwiftPM.
+        // We do not want to include `ExampleModel` in our package.
+        // However, this is not that bad.
+        // We still run tests via the main Xcode project.
+
+        // .testTarget(name: "JSQCoreDataKitTests", dependencies: ["JSQCoreDataKit"], path: "Tests")
     ],
     swiftLanguageVersions: [.v5]
 )
